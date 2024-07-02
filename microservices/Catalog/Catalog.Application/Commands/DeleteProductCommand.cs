8f00d4323e0f891da0b94c7e56a5e65d0d553483
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-07-02 11:46:54 +0200

    Fixed Delete Command and Handler Error

---
+++ microservices/Catalog/Catalog.Application/Commands/DeleteProductCommand.cs
@@ -0,0 +1,14 @@
+using MediatR;
+
+namespace Catalog.Application.Commands
+{
+    public class DeleteProductCommand : IRequest<bool>
+    {
+        public string Id { get; set; }
+
+        public DeleteProductCommand(string id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ microservices/Catalog/Catalog.Application/Handlers/DeleteProductCommandHandler.cs
@@ -0,0 +1,20 @@
+using Catalog.Application.Commands;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, bool>
+    {
+        private readonly IProductRepository _productRepository;
+        public DeleteProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            return await _productRepository.DeleteProductAsync(command.Id);
+        }
+    }
+}
