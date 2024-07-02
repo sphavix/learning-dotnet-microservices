c9f7716bb64756defaa2978f288fff71dbd85b2b
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-07-02 11:57:52 +0200

    Added Api Controller with Errors

---
+++ microservices/Catalog/Catalog.API/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Catalog.API.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+    }
+}
