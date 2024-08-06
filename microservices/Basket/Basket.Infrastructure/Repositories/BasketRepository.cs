95396d97522421ac03e6236c6872529c8b75f68b
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-08-06 10:19:52 +0200

    Added Basket Repository

--- microservices/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
+++ microservices/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -2,7 +2,7 @@
 
 namespace Basket.Infrastructure.Repositories
 {
-    public class BasketRepository
+    public class BasketRepository : Basket.Core.Repositories.IShoppingCartRepository
     {
         
     }
